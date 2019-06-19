2324a72c091081d650b3a49a274754969b69aca5
praveenatfb <praveen.gkcemca@gmail.com>
2019-06-19 23:04:20 +0530

    selectionsort.js

--- nodedemos/selectionsort.js
+++ nodedemos/selectionsort.js
@@ -1,1 +1,12 @@
-let elements = [5,8,2,9,3,1,4,6];
+let elements = [5, 8, 2, 9, 3, 1, 4, 6];
+console.log("The array elements before sorting are " + elements);
+for (var i = 0; i < elements.length - 1; i++) {
+    for (var j = i + 1; j < elements.length; j++) {
+        if (elements[i] > elements[j]) {
+            var temp = elements[i];
+            elements[i] = elements[j];
+            elements[j] = temp;
+        }
+    }
+}
+console.log("The array elements are::" + elements);
