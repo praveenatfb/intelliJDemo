e1befd509aec00a2dd5a58c9f8c9ce7c874d907f
praveenatfb <praveen.gkcemca@gmail.com>
2019-06-19 23:27:22 +0530

    modified

--- DemoProject/src/main/java/com/core/SelectionSort.java
+++ DemoProject/src/main/java/com/core/SelectionSort.java
@@ -3,20 +3,22 @@ package com.core;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int []num= {4,6,2,1,8,7};
-		int small=0,swap=0,i,j;
-		for(i=0;i<num.length-1;i++)
-			for(j=i+1;num[j]<num.length;j++) {
-				if(num[i]>num[j])
-				//i=j;
-			swap=num[i];
-			num[i]=num[j];
-			num[j]=swap;
-			}
-			for(i=0;i<num.length;i++) {
-				//for( j=i-1;j>i;j--) {
-					System.out.println(num[i]);
+		int[] num = { 4, 6, 2, 1, 8, 7 };
+		int small = 0, swap = 0, i, j;
+		for (i = 0; i < num.length - 1; i++)
+			for (j = i + 1; j < num.length; j++) {
+				if (num[i] > num[j]) {
+					// i=j;
+					swap = num[i];
+					num[i] = num[j];
+					num[j] = swap;
 				}
+			}
+		System.out.println("array elements are "+num.toString());
+		for (i = 0; i < num.length; i++) {
+			// for( j=i-1;j>i;j--) {
+			System.out.print(num[i]+" ");
+		}
 	}
 
 }
