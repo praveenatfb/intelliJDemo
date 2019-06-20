20633a4bfaa2cdb0e3760633c14e1f6783b6bae8
mohammadGafar <smohammadgafar@gmail.com>
2019-06-20 11:32:56 +0530

    errors rectified

--- DemoProject/src/main/java/com/core/SelectionSort.java
+++ DemoProject/src/main/java/com/core/SelectionSort.java
@@ -6,16 +6,17 @@ public class SelectionSort {
 		int []num= {4,6,2,1,8,7};
 		int small=0,swap=0,i,j;
 		for(i=0;i<num.length-1;i++)
-			for(j=i+1;num[j]<num.length;j++) {
-				if(num[i]>num[j])
+			for(j=i+1;j<num.length;j++) {
+				if(num[i]>num[j]) {
 				//i=j;
 			swap=num[i];
 			num[i]=num[j];
 			num[j]=swap;
+				}
 			}
 			for(i=0;i<num.length;i++) {
 				//for( j=i-1;j>i;j--) {
-					System.out.println(num[i]);
+					System.out.println(num[i]+" ");
 				}
 	}
 
