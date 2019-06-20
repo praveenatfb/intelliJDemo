d9d8094eefbc82fcdfc876356dd86861778beea8
mohammadGafar <smohammadgafar@gmail.com>
2019-06-20 14:20:01 +0530

    new insertion commit

--- DemoProject/src/main/java/com/core/InsertionSort.java
+++ DemoProject/src/main/java/com/core/InsertionSort.java
@@ -3,22 +3,21 @@ package com.core;
 public class InsertionSort {
 
 	public static void main(String[] args) {
-		int []n= {7,2,6,1,8,9};
-		int swap=0,min=0,i,j;
-		for (i=0;i<n[i]-1;i++)
-			min=i;
-			for(j=i+1;j<n[j];j++) {
-				if(n[i]>n[j]) {
-					min=j;
-				}}
-				swap=n[min];
-				n[min]=n[j];
-				n[j]=swap;
-		//		}
-		//	}
-			for(i=0;i<n.length;i++) {
-				System.out.println(n[i]+" ");
-			}
+		int[] num = { 7, 2, 6, 1, 8, 9 };
+		int temp = 0, i, j;
+		for (i = 0; i < num.length; i++) {
+			temp = num[i];
+		j = i;
+		
+		while (j > 0 && temp < num[j - 1]) {
+			num[j] = num[j - 1];
+			j = j - 1;
+		}
+		num[j] = temp;
+	
 	}
-
-}
+		for (i = 0; i < num.length; i++) {
+			System.out.print(num[i] + " ");
+		}
+	}
+	}
