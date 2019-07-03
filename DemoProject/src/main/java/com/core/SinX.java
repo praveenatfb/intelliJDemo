036380e4c36760687f4d6552e719dabc04e128f4
mohammadGafar <smohammadgafar@gmail.com>
2019-07-03 17:20:45 +0530

    updated sinX prog

--- DemoProject/src/main/java/com/core/SinX.java
+++ DemoProject/src/main/java/com/core/SinX.java
@@ -13,10 +13,24 @@ public class SinX {
 		for(int i=1;i<=n;i++) {
 			x=i;
 			int y=x;
-			sum+=Math.pow(x,y)/y;
+			sum+=power(x,y)/factorial(y);
 			System.out.println(sum);
 		}
 		System.out.println("sinX value is:"+sum);
+	}
+	public static double power(int x,int y) {
+		double pow=1;
+		for(int i=0;i<y;i++) 
+			pow=pow*x;
+		System.out.println("in pow"+pow);
+		return pow;
+	}
+	public static int factorial(int y) {
+		int fact=1;
+		for(int i=1;i<=y;i++)
+		fact = fact*i;
+		System.out.println("in fact"+fact);
+		return fact;
 	}
 
 }
